ae251447c12ae10c5346bd8e4380a360e5ea65ac
Luigi-Faldetta <luigifaldetta@gmail.com>
2025-04-12 23:00:18 +0200

    fix: deleted old allowed origins in cors

--- ai_agent/atlasScript.py
+++ ai_agent/atlasScript.py
@@ -19,7 +19,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://atlasnew-git-main-luigifaldettas-projects.vercel.app/", "https://atlasnew.vercel.app/"],  # Allow requests from your frontend
+    allow_origins=["https://atlasnew.vercel.app", "http://localhost:3000"],  # Allow requests from your frontend
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
