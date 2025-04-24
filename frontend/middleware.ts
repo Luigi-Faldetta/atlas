c63614787cc2dfd553f14057b3e5bcb741759cd3
Luigi-Faldetta <luigifaldetta@gmail.com>
2025-04-24 21:09:34 +0200

    feat: testing middleware loading in production

---
+++ frontend/middleware.ts
@@ -0,0 +1,32 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+// Define protected routes using regex patterns.
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/properties(.*)',
+  '/watchlist(.*)',
+  '/tools(.*)',
+]);
+
+console.log(process.env.NEXT_PUBLIC_CLERK_DOMAIN);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    // If the user is not logged in and the route is protected, redirect to sign-in.
+    return redirectToSignIn();
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
